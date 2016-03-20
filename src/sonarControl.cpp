723eed0b7346a51ce1f3addf442eccfa94868757
GonzalesMK <judcnegri@gmail.com>
2016-03-20 16:26:18 -0300

    forth commit

--- src/sonarControl.cpp
+++ src/sonarControl.cpp
@@ -5,23 +5,46 @@
 
 ros::Publisher pub;
 geometry_msgs::Twist vel;
+int teste_left,teste_right; //usado para levar informações entre os sensores 
+                            // se o sensor estiver bloqueado teste = 1
 
-void callback(const std_msgs::Float32ConstPtr& dist) {
-   if (dist->data != 0 ) {
-         vel.linear.x = -10;
-         vel.angular.z =10;
-     } else{ 
-         vel.linear.x = 10;
-         vel.angular.z = 0;
-     }
+void cb_front(const std_msgs::Float32ConstPtr& dist) {
+   if (dist->data != 0) { // se o sensor marcar alguma coisa, o carro vai devagar
+         vel.linear.x = 0.5;
+  } else vel.linear.x = 3;        // senão, ele corre
+    
+    if (!( teste_left || teste_right) ) vel.angular.z = 0; //se nenhum dos sensores laterais apitar, o carro vai reto
     pub.publish(vel);
+}
+
+void cb_left(const std_msgs::Float32ConstPtr &dist )
+{
+    if ( dist->data != 0){         // se o sensor da direita apitar, o carro vira
+        vel.angular.z = -3 ;   
+        pub.publish(vel);
+        teste_left = 1;         // se o sensor apitar 
+    }  else teste_left = 0 ;
+}
+
+void cb_right(const std_msgs::Float32ConstPtr &dist ) // se o sensor do carro apitar, o carro vira
+{
+    if ( dist->data != 0){
+        vel.angular.z = 3 ;
+        pub.publish(vel);
+        teste_right=1;
+    } else teste_right =0;
+    
 }
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "sonarController");
     ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("/vrep/vehicle/frontSonar", 1, callback);
-    pub = n.advertise<geometry_msgs::Twist>("/sonarController", 1);
+    teste_left=0;
+    teste_right=0;
+    ros::Subscriber sub_front = n.subscribe("/vrep/vehicle/frontSonar", 1, cb_front); //topico para o sensor da frente
+    ros::Subscriber sub_left = n.subscribe("/vrep/vehicle/leftSonar", 1, cb_left); //topico left
+    ros::Subscriber sub_right = n.subscribe("/vrep/vehicle/rightSonar", 1, cb_right); //topico para o sensor right
+    pub = n.advertise<geometry_msgs::Twist>("/sonarController", 1); //topico para comunicar com Kinect
     
     ros::spin();
     
